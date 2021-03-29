bd39c79cb0116e748fd76f6caa93f8ea4c6ab79f
connorjbryant <connorjamesbryant7@gmail.com>
2021-03-29 00:29:01 -0400

    Committing to main

---
+++ Assets/Scripts/BallFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFollow : MonoBehaviour
+{
+    GameObject target;
+    float speed;
+    public Transform Player;
+    public Transform target2;
+
+    void Start()
+    {
+        speed = 3f;
+        target = GameObject.Find("Paddle");
+    }
+
+    void Update()
+    {
+
+        Vector3 lookVector = Player.transform.position - transform.position;
+        lookVector.y = transform.position.y;
+        Quaternion rot = Quaternion.LookRotation(lookVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Random.Range(5, 20) * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/DelayedStart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class DelayedStart : MonoBehaviour
+{
+    public GameObject countDown;
+
+    public void Start()
+    {
+        StartCoroutine("StartDelay");
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void StartAnimation()
+    {
+        StartCoroutine("StartDelay");
+    }
+
+    IEnumerator StartDelay()
+    {
+        Time.timeScale = 0;
+        GameObject.FindWithTag("Ball").GetComponent<BallStart>().enabled = false;
+        float pauseTime = Time.realtimeSinceStartup + 7f;
+        while (Time.realtimeSinceStartup < pauseTime)
+            yield return 0;
+        countDown.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        GameObject.FindWithTag("Ball").GetComponent<BallStart>().enabled = true;
+    }
+}
